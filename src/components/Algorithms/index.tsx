e99b88571f2b541df4b4f5b414a5794c1e351320
Mar Calmon <marical2222@gmail.com>
2020-11-23 17:28:08 -0300

    Algorithms.

--- src/components/Algorithms/index.tsx
+++ src/components/Algorithms/index.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 // Implement a form that given a word (string) returns whether thar word is "unique"
 // "unique" means no character in the word is repeated.
 // e.g. unique("hello") --> false
 // e.g. unique("algorithm") --> true
 function Algorithms(){
+  const [name, setName] = useState('');
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  }
+
+  const unique = ({value}: any) => {
+    let result = 'Es única';
+    const wordArray = value.split('');
+    for (let j=0; j < wordArray.length; j ++) {
+      const letter = wordArray[j];
+      for (let i=j+1; i < wordArray.length; i ++) {
+        if(letter === wordArray[i]) {
+          result = 'No es única'
+        }
+      }
+    }
+    console.log(result)
+  }
+  // doble for
+  // casos bordes  - condiciones a cumplir para que no se rompe
+  // booleano
+  // mostrar
+  // funcion que me devuelva si un string un anagrama
+  // otro componente con dos inputs
+
+  const handleChange = ({value}: any) => {
+    setName(value);
+    console.log(value)
+  }
+
   return (
-    <div />
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <h1>Palabras únicas</h1>
+        <input type="text" name={name} onChange={e => handleChange(e.target)} className="input"/>
+        <button type="submit" className="button">Verificar</button>
+      </form>
+      <p>Es única:</p>
+    </div>
   )
 }
 
